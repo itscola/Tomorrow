588d4fbde4599ace1a7df288044f18bfefa92fe2
QianXia <1928650539@qq.com>
2021-07-26 18:14:16 +0800

    fix relaod

--- tomorrow/tomo/commands/commands/Reload.java
+++ tomorrow/tomo/commands/commands/Reload.java
@@ -1,9 +1,13 @@
 package tomorrow.tomo.commands.commands;
 
+import java.util.Iterator;
+
 import tomorrow.tomo.Client;
 import tomorrow.tomo.commands.Command;
 import tomorrow.tomo.customgui.CustomGuiManager;
+import tomorrow.tomo.event.EventBus;
 import tomorrow.tomo.managers.ModuleManager;
+import tomorrow.tomo.mods.Module;
 import tomorrow.tomo.utils.cheats.player.Helper;
 
 public class Reload extends Command {
@@ -13,10 +17,18 @@ public class Reload extends Command {
 
     @Override
     public String execute(String[] var1) {
+    	// unregister all the mods
+    	for (Module mod : ModuleManager.enabledModules) {
+			EventBus.getInstance().unregister(mod);
+		}
+    	// clear the modules lists
         ModuleManager.modules.clear();
         CustomGuiManager.objects.clear();
         ModuleManager.enabledModules.clear();
-
+        // unregister ModuleManager::onKey....
+        EventBus.getInstance().unregister(Client.instance.getModuleManager());
+        
+        // reload modules
         Client.instance.getModuleManager().init();
         Helper.sendMessage("Reloaded");
         return null;
